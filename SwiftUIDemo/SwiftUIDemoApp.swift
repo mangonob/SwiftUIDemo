cd94432a60eeb0eb6c8170abdc034f35d10cb6bb
mangonob <mangonob@qq.com>
2021-03-18 10:25:02 +0800

    build: Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by G on 2021/3/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
