25a6e7469404fc345d8f70e7ceb5f7fca2ce4e3c
mangonob <mangonob@qq.com>
2021-03-18 14:44:07 +0800

    feat: Building Lists and Navigation

---
+++ SwiftUIDemo/Views/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkList.swift
+//  SwiftUIDemo
+//
+//  Created by G on 2021/3/18.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
